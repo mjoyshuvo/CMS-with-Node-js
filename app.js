1a6ee608a696c178ff00e60fe56b5d52a3ea5b05
mjoyshuvo <mjoyshuvo@gmail.com>
2018-03-17 12:25:17 +0600

    template engine added

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-//mjoyshuvo
+const express = require('express');
+const app = express();
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+app.use(express.static(path.join(__dirname, 'public'))); //-----static files-----
+
+app.engine('handlebars', exphbs({ defaultLayout: 'home' }));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    res.render('home/index');
+});
+
+app.listen(4500, () => {
+    console.log('-----listening on port--------');
+});
